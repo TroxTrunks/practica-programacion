b5c0a41cb88395dc45a39a55482b354243844f75
TroxTrunks <carquinpar01@gmail.com>
2025-08-14 19:58:42 -0400

    termine mi proyecto

---
+++ script.js
@@ -0,0 +1,37 @@
+let numero1 = 20
+let numero2 = 80
+function restar(n1,n2){
+   let num1 = n1
+   let num2 = n2
+   let resultado = num1 - num2
+   return resultado 
+   
+} 
+
+
+
+function dividir(n1,n2){
+let num1 = n1
+let num2 = n2
+let resultado = num1/num2
+return resultado
+     
+}
+ 
+function multiplicar(n1,n2){
+    let num1 = n1
+    let num2 = n2
+    let resultado = num1*num2
+    return resultado
+}
+
+function sumar(n1,n2){
+    let num1 = n1 
+    let num2 = n2
+    let resultado = num1+num2
+    return resultado
+}
+alert(sumar(20,60))// 80
+alert(restar(20,60))// -40
+alert(multiplicar(2,5))// 10
+alert(dividir(20,2)) // 10
